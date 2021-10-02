3d2d272b58b0ffa3d20f4e9b8a094497994db121
Wences81 <zhylinpochta@gmail.com>
2021-10-02 18:35:28 +0200

    day3.1

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,30 @@
+import toast from 'react-hot-toast';
+import { Button, Container, Input } from './SearchBar.styled';
+
+export default function SearchBar({ onSearch }) {
+  const handleSearch = e => {
+    e.preventDefault();
+    if (e.target.elements.searchName.value.toLowerCase().trim() === '') {
+      return toast.error('The search field is empty!');
+    }
+    onSearch(e.target.elements.searchName.value.toLowerCase());
+    e.target.reset();
+  };
+
+  return (
+    <Container>
+      <from onSubmit={handleSearch}>
+        <Input
+          name="searchName"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Enter movie name"
+        />
+        <Button type="submit">
+          <span>Search</span>
+        </Button>
+      </from>
+    </Container>
+  );
+}
---
+++ src/components/SearchBar/SearchBar.styled.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+export const Container = styled.div`
+  margin-top: 20px;
+`;
+
+export const Button = styled.button`
+  padding: 10px 14px;
+  margin-left: 10px;
+  border-radius: 5px;
+  box-shadow: 5px 6px 9px rgb(85, 42, 85);
+`;
+
+export const Input = styled.input`
+  padding: 10px 14px;
+  border-radius: 5px;
+  box-shadow: 5px 6px 9px rgb(85, 42, 85);
+`;
